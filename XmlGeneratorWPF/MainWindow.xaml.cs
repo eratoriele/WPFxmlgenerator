b2ec63f31f305ac0bbc3a7d4889b5617e0512092
eratoriele <boragulerman@gmail.com>
2019-08-05 20:45:59 +0300

    Added 2 click events, textblock and boxes, also they turn invisible now

--- XmlGeneratorWPF/MainWindow.xaml.cs
+++ XmlGeneratorWPF/MainWindow.xaml.cs
@@ -27,17 +27,60 @@ namespace XmlGeneratorWPF
 
         private void OperationButton_Click(object sender, RoutedEventArgs e)
         {
+            turnAllInvisible();
 
         }
 
         private void PlateRecognitionButton_Click(object sender, RoutedEventArgs e)
         {
+            turnAllInvisible();
+            // Set required fields to visible
+            fromTextBox.Visibility = Visibility.Visible;
+            fromTextBlock.Visibility = Visibility.Visible;
+            toTextBox.Visibility = Visibility.Visible;
+            toTextBlock.Visibility = Visibility.Visible;
+            dPathTextBlock.Visibility = Visibility.Visible;
+            dPathTextBox.Visibility = Visibility.Visible;
 
+            plateRecognitionButton.IsEnabled = false;
+
+            fromTextBox.Focus();
         }
 
         private void SecurityRecordingButton_Click(object sender, RoutedEventArgs e)
         {
+            turnAllInvisible();
+            // Set required fields to visible
+            fromTextBox.Visibility = Visibility.Visible;
+            fromTextBlock.Visibility = Visibility.Visible;
+            toTextBox.Visibility = Visibility.Visible;
+            toTextBlock.Visibility = Visibility.Visible;
+            sPathTextBlock.Visibility = Visibility.Visible;
+            sPathTextBox.Visibility = Visibility.Visible;
+            dPathTextBlock.Visibility = Visibility.Visible;
+            dPathTextBox.Visibility = Visibility.Visible;
 
+            securityRecordingButton.IsEnabled = false;
+
+            fromTextBox.Focus();
+        }
+
+        // Turns every button except for 3 main buttons to invisible, so that when a new one is selected,
+        // Only the relevant ones are shown
+        private void turnAllInvisible()
+        {
+            fromTextBox.Visibility = Visibility.Collapsed;
+            fromTextBlock.Visibility = Visibility.Collapsed;
+            toTextBox.Visibility = Visibility.Collapsed;
+            toTextBlock.Visibility = Visibility.Collapsed;
+            sPathTextBlock.Visibility = Visibility.Collapsed;
+            sPathTextBox.Visibility = Visibility.Collapsed;
+            dPathTextBlock.Visibility = Visibility.Collapsed;
+            dPathTextBox.Visibility = Visibility.Collapsed;
+
+            operationButton.IsEnabled = true;
+            plateRecognitionButton.IsEnabled = true;
+            securityRecordingButton.IsEnabled = true;
         }
     }
 }
